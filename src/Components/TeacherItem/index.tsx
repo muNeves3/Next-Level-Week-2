dae1699cbb6928eaf32c921b5af213e4469ea5a5
muNeves3 <murilogrilosn@gmail.com>
2020-08-04 16:39:55 -0300

    Reused components

---
+++ src/Components/TeacherItem/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars1.githubusercontent.com/u/62778436?s=460&u=e006381212316eb35d33a7321258e8d2c7825efa&v=4"
+          alt="Murilo Neves"
+        />
+        <div>
+          <strong>Murilo Neves</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratórios e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 100,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="WhatsApp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
